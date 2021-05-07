8c04f97beb6769e119c17e4b398428603d914d90
Ben Quirk <bquirk@centerline.net>
2021-05-07 17:07:18 -0400

    feat(tutorial): complete step 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import './app.scss';
+// note: I don't see Content as a component in this library anymore, deprecated/merged I guess.
+import { Content } from 'carbon-components-react';
+import TutorialHeader from './components/TutorialHeader';
+import { Route, Switch } from 'react-router-dom';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
+      <>
+        <TutorialHeader />
+        <Content>
+          {/* Note basic routing pattern here using switch and route components
+               where you define right in the props the URL and component to show at that route. Very cool. */}
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/repos" component={RepoPage} />
+          </Switch>
+        </Content>
+      </>
     );
   }
 }
---
+++ src/components/TutorialHeader/index.js
@@ -0,0 +1,5 @@
+// https://www.carbondesignsystem.com/developing/react-tutorial/step-1/
+// index.js
+
+import TutorialHeader from './TutorialHeader';
+export default TutorialHeader;
